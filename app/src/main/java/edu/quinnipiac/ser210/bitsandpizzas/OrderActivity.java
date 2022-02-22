2e8cfc7020b9bd7146f51988dd7f69af07e5fb68
juliawoeste <75594788+juliawoeste@users.noreply.github.com>
2022-02-21 22:30:55 -0500

    chapter 8 bits and pizzas

---
+++ app/src/main/java/edu/quinnipiac/ser210/bitsandpizzas/OrderActivity.java
@@ -0,0 +1,20 @@
+package edu.quinnipiac.ser210.bitsandpizzas;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
